71064a9bcc7bbd5bc45a5122234e60e49610a5d0
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-04-14 14:29:44 -0500

    config database

--- src/config/config.js
+++ src/config/config.js
@@ -19,6 +19,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     
     urlDB = 'mongodb://localhost/notes-db-app';
+}else {
+
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
